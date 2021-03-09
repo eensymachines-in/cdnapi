273750394a9d161d5ac43a754e8f388fb35bcaba
Niranjan Awati <kneerunjun@gmail.com>
2021-03-09 19:26:17 +0530

    +code for CORS enablement

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		// First, we add the headers with need to enable CORS
+		// Make sure to adjust these headers to your needs
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Content-Type", "application/json")
+		// Second, we handle the OPTIONS problem
+		if c.Request.Method != "OPTIONS" {
+			c.Next()
+		} else {
+			// Everytime we receive an OPTIONS request,
+			// we just return an HTTP 200 Status Code
+			// Like this, Angular can now do the real
+			// request using any other method than OPTIONS
+			c.AbortWithStatus(http.StatusOK)
+		}
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	// mapping all the popular css and js statics onto a static route
+	// https://developers.google.com/web/fundamentals/performance/get-started/httpcaching-6
 	static := r.Group("/static")
 	static.Static("/jquery", "node_modules/jquery/dist")
 	static.Static("/popper", "node_modules/popper.js/dist")
